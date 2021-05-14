d598c614839ce6ea08da0951df5d2d50043cb196
Rohan Kacheria <rohankacheriam@gmail.com>
2021-05-14 22:39:51 +0530

    productList and productDetail Reducers

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,23 +1,26 @@
-import React, {useState,useEffect} from 'react';
-import Product from "../Components/Product";
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import Product from "../Components/Product.js";
 import {Row, Col} from "react-bootstrap";
-import axios from "axios";
+import {listProducts} from "../Actions/productActions.js";
+import Message from "../Components/Message.js";
+import Loader from "../Components/Loader.js";
 
 const HomeScreen = () => {
-    const [products,setProducts]=useState([]);
+    const dispatch=useDispatch();
+
+    const  productList=useSelector(state=>state.productList);
+    const {loading, error, products}=productList;
 
     useEffect(()=>{
-        const fetchProducts=async ()=>{
-            const {data}=await axios.get("/api/products");
-            setProducts(data);
-        }
-        fetchProducts();
-    },[]);
+        dispatch(listProducts());
+    },[dispatch]);
 
     return (
         <>
             <h1>Latest Products</h1>
-            <Row>
+            {loading ? <Loader />:error?<Message variant="danger">{error}</Message>:
+                <Row>
                 {
                     products.map((product)=>(
                         <Col key={product._id} sm={12} lg={4} md={6} xl={3}>
@@ -26,6 +29,8 @@ const HomeScreen = () => {
                     ))
                 }
             </Row>
+            }
+            
         </>
     )
 }
